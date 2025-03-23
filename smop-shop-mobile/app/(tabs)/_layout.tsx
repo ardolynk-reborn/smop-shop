ddc9995ac992086aabc935601f02c2ac1d82ae0c
Michael Tikhonenko <ardolynk75@gmail.com>
2025-03-23 02:10:29 +0100

    Total amount badge added to the cart tab

--- smop-shop-mobile/app/(tabs)/_layout.tsx
+++ smop-shop-mobile/app/(tabs)/_layout.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Stack, Tabs } from 'expo-router'
+import { Tabs } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
+import { CartContext } from '../providers/CartProvider';
 
 export default function _layout() {
+
+  const { cart } = React.useContext(CartContext);
+
   return (
     <Tabs
       screenOptions={{
@@ -12,14 +16,17 @@ export default function _layout() {
     >
       <Tabs.Screen name="(products)"
         options={{
-          tabBarIcon: ({ size, color }) => <Ionicons name="storefront-outline" size={size} color={color}/>
+          tabBarIcon: ({ size, color }) => <Ionicons name="storefront-outline" size={size} color={color}
+        />
         }}
       />
       <Tabs.Screen name="cart"
         options={{
-          tabBarIcon: ({ size, color }) => <Ionicons name="cart-outline" size={size} color={color}/>
+          tabBarIcon: ({ size, color }) => <Ionicons name="cart-outline" size={size} color={color}/>,
+          tabBarBadge: cart.length == 0 ? undefined : cart.reduce((total, item) => total + item.quantity, 0)
         }}
-      />
+      >
+      </Tabs.Screen>
     </Tabs>
-  )
+ )
 }
